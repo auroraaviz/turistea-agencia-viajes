b70829b62fe50fd7711a0c9e8cb241aeccd302dc
Aurora-Medac <tucorreo@centroestudios.comaai0003@alu.medac.es>
2026-04-14 19:42:55 +0200

    Mejora de filtros

---
+++ frontend/js/filtros/filtros.js
@@ -0,0 +1,68 @@
+//esperar que el documento esté cargado
+document.addEventListener('DOMContentLoaded', function() {
+
+    const btnBuscar = document.querySelector('.btn-buscar');
+    btnBuscar.addEventListener('click', function() {
+
+        //Recoge los valores de los inputs
+        const destino = document.getElementById('filtro-destino').value.trim();
+        const fechaInicioInput = document.getElementById('fecha-inicio').value;
+        const fechaFinInput = document.getElementById('fecha-fin').value;
+
+        //Aplica margen de +/- 7 dias
+        let fechaInicioFinal = null;
+        let fechaFinFinal = null;
+
+        //fecha de inicio -7 dias
+        if (fechaInicioInput) {
+            const fecha = new Date (fechaInicioInput);
+            fecha.setDate(fecha.getDate() - 7); 
+            fechaInicioFinal = fecha;
+        }
+
+        //fecha de fin +7 dias
+        if (fechaFinInput) {
+            const fecha = new Date (fechaFinInput);
+            fecha.setDate(fecha.getDate() + 7);
+            fechaFinFinal = fecha;
+        }
+
+        //Recoge todas las tarjetas de paquetes
+        const paquetes = document.querySelectorAll('.card');
+
+        paquetes.forEach(function (paquete) {
+
+            let mostrar = true;
+
+            //Filtro por destino
+            const titulo = paquete.querySelector('.card-title');
+            if (destino && titulo) {
+                if (!titulo.textContent.toLowerCase().includes(destino.toLowerCase())) {
+                    mostrar = false;
+                }
+            }
+
+            //Filtro por fechas
+            const fechaInicioPaquete = paquete.dataset.fechaInicio;
+            const fechaFinPaquete = paquete.dataset.fechaFin;
+
+            
+            if(fechaInicioFinal && fechaInicioPaquete) {
+                const fechaPaquete = new Date(fechaInicioPaquete);
+                if (fechaPaquete < fechaInicioFinal) {
+                    mostrar = false;
+                }
+            }
+
+            if (fechaFinFinal && fechaFinPaquete) {
+                const fechaPaquete = new Date(fechaFinPaquete);
+                if (fechaPaquete > fechaFinFinal) {
+                    mostrar = false;
+                }
+            }
+
+            //Mostrar u ocultar el paquete
+            paquete.closest('.col-12').style.display = mostrar ? '' : 'none';
+        });
+    });
+});
