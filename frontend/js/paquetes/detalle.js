f0e04259b914426202938c8145829fbc66eda4ad
jgm0089-lab <jgm0089@alu.medac.es>
2026-04-21 19:11:44 +0200

    fix: faltaba un import

--- frontend/js/paquetes/detalle.js
+++ frontend/js/paquetes/detalle.js
@@ -1,5 +1,6 @@
 
 import { obtener } from "../utils/fetch.js";
+import { BASE } from "../config.js";
 console.log("detalle.js cargado");
 document.addEventListener("DOMContentLoaded", async () => {
   const loading = document.getElementById("loading");
